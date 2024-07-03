d3390942775e6a22e727b485f99191bf40b34d95
Nate <nate@natestuff.com>
2024-07-02 19:57:21 -0500

    update key stuff so i dont have to worry about pushing it
    title

--- keys.py
+++ keys.py
@@ -1,3 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 open_api_weather_key= 'your open api key here'
 connection_string = 'mongodb://localhost:27017'
-bot_key = 'MTI0MDAzMzc1Nzc3NjU4MDY3OA.G-nDmo.Aq2mGhFHphINyKwHffx0pz-qn_BkcwgdXOx6IY'
+bot_key = os.getenv("bot_key")
